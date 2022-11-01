98519c29f0f69975407f7099f3988fc176a65dba
Samuel Gonzalez Rodriguez <ing.samuelgonzalez@hotmail.com>
2022-10-31 21:43:19 -0400

    Finish Date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+	Console.WriteLine(new Date(2024, 02, 29));
+	Console.WriteLine(new Date(1974, 11, 30));
+	Console.WriteLine(new Date(2022, 04, 05));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
